8e72fe218796400da224eb19f2ffd0753dd777d9
Miguel Fernandez <fernandez.s.miguel96@gmail.com>
2019-06-21 11:08:57 +0200

    v0.0.1 - Commmit inicial

---
+++ springmodule/src/main/java/com/miguel/springmodule/SpringmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.springmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmoduleApplication extends ServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmoduleApplication.class, args);
+    }
+
+}
